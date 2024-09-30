a415328cfe22286eea4becbfeb4cfd6948f22758
Hrishi-MiM <shinde@madeinmars.com>
2024-09-30 13:13:02 -0700

    Updated call serializer to send assistant's details.

--- calls/serializers.py
+++ calls/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Call, Log
+from assistant.serializers import AssistantSerializer
 
 class CallSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source='created_by.username')
+    assistant_data = AssistantSerializer(source='assistant', read_only=True)
 
     class Meta:
         model = Call
-        fields = '__all__'
+        fields = ('id', 'session_name', 'assistant', 'assistant_data', 'created_by', 'created_on', 'updated_on')
         read_only_fields = ('created_by', 'created_on', 'updated_on')
 
 class LogSerializer(serializers.ModelSerializer):
