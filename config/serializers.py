13aaaa94748f34cba48330988da06aee569bd256
Hrishi-MiM <shinde@madeinmars.com>
2024-10-01 08:44:45 -0700

    Added assistants in twilio config.

--- config/serializers.py
+++ config/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import TwilioConfig
+from assistant.models import Assistant
+
+class AssistantSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assistant
+        fields = ['id', 'name']  # Include only the fields you need
 
 class TwilioConfigurationSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source='created_by.username')
+    assistants = AssistantSimpleSerializer(many=True, read_only=True)  # Use nested serializer
 
     class Meta:
         model = TwilioConfig
